873a982f77af251f4268b822330ac7a5a675b821
debutomberi <asahii.jr@gmail.com>
2018-10-26 11:23:37 +0900

    必要なファイルを用意とSingletonMonoBehaviorを用意

--- Assets/Menber/Asahi/Test.cs
+++ Assets/Menber/Asahi/Test.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sada : MonoBehaviour {
+public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Script/SingletonMonoBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SingletonMonoBehavior<T> : MonoBehaviour where T : MonoBehaviour{
+
+
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);
+                if (instance == null)
+                {
+                    Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        // 他のGameObjectにアタッチされているか調べる.
+        // アタッチされている場合は破棄する.
+        if (this != Instance)
+        {
+            Destroy(this);
+            //Destroy(this.gameObject);
+            Debug.LogError(
+                typeof(T) +
+                " は既に他のGameObjectにアタッチされているため、コンポーネントを破棄しました." +
+                " アタッチされているGameObjectは " + Instance.gameObject.name + " です.");
+            return;
+        }
+
+    }
+
+
+}
