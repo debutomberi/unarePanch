0ff141cf124d35ed57dec5c1bc5c416e7ee596ed
asahi <asahii.jr@gmail.com>
2018-10-29 12:06:51 +0900

    テストスクリプトの微修正

--- Assets/Menber/Asahi/Script/Test.cs
+++ Assets/Menber/Asahi/Script/Test.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Test : MonoBehaviour {
 
     [SerializeField]
-    int[] AttackFlame = { 20, 20, 20 };
+    Collider2D attackCollider;
+    [SerializeField]
+    int[] attackFlame = { 20, 20, 20 };
+
 
 	// Use this for initialization
 	void Start () {
