a34334766c1359774a5e5c58d9d3e7e66a92f54f
兎子-PC\兎子 <asahii.jr@gmail.com>
2018-11-01 22:48:52 +0900

    攻撃の当たり判定の取り方を変更

--- Assets/Menber/Asahi/Script/AttackCollider.cs
+++ Assets/Menber/Asahi/Script/AttackCollider.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class AttackCollider : MonoBehaviour {
 
-    private void OnCollisionEnter2D(Collision2D collision){
+    private void OnTriggerEnter2D(Collider2D collision){
         var test = transform.parent.GetComponent<Test>();
         test.hit();
     }
-
-    private void OnCollisionStay2D(Collision2D collision){
-        
-    }
+    
 }
