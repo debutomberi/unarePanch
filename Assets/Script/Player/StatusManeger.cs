6ca73379ecbfa00d829fd0964d86878cd3e8e652
兎子-PC\兎子 <asahii.jr@gmail.com>
2018-11-06 00:12:21 +0900

    リストをチェックする関数を作成

--- Assets/Script/Player/StatusManeger.cs
+++ Assets/Script/Player/StatusManeger.cs
@@ -18,6 +18,10 @@ public class StatusManeger : SingletonMonoBehavior<StatusManeger> {
 
     }
 
+    //各最終コマンドのチェック
+    int onePlayerKey = 5;
+    int twoPlayerKey = 5;
+
     //コマンド履歴を消すまでの時間
     int timerCountOne;
     int timerCountTwo;
@@ -26,26 +30,53 @@ public class StatusManeger : SingletonMonoBehavior<StatusManeger> {
 
     private void Update(){
         DeleteCommand();
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+            SetCommandOnePlayer(4);
+        if (Input.GetKeyDown(KeyCode.S))
+            SetCommandOnePlayer(6);
+        if (Input.GetKeyDown(KeyCode.F))
+            SetCommandTwoPlayer(6);
+        if (Input.GetKeyDown(KeyCode.D))
+            SetCommandTwoPlayer(4);
+        if (Input.GetKeyDown(KeyCode.W))
+            Debug.Log(CheckCommandOnePlayer());
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log(CheckCommandTwoPlayer());
+        }
+        
     }
     
     //1Pのコマンド履歴に追記
     public void SetCommandOnePlayer(int command){
-
+        //コマンドが押しっぱなしならそのまま返す
+        if (onePlayerKey == command) {
+            Debug.Log("同じキーやで");
+            return;
+        }
         if (onePlayerCommandList.Count <= 1) {
             timerCountOne = 0;
         }
         onePlayerCommandList.Add(command);
-        
+        Debug.Log("1Pコマンド：" + command);
+        onePlayerKey = command;
     }
     
     //2Pのコマンド履歴に追記
     public void SetCommandTwoPlayer(int command){
+        //コマンドが押しっぱなしならそのまま返す
+        if (twoPlayerKey == command) {
+            Debug.Log("同じキーやで");
+            return;
+        }
 
         if (twoPlayerCommandList.Count <= 1) {
             timerCountTwo = 0;
         }
         twoPlayerCommandList.Add(command);
-        
+        Debug.Log("2Pコマンド：" + command);
+        twoPlayerKey = command;
     }
     
     //コマンド履歴の削除
@@ -54,18 +85,63 @@ public class StatusManeger : SingletonMonoBehavior<StatusManeger> {
         timerCountTwo++;
         if(timerCountOne >= timer) {
             timerCountOne = 0;
-            if(onePlayerCommandList.Count > 1) {
+            if(onePlayerCommandList.Count > 2) {
                 onePlayerCommandList.RemoveAt(0);
                 Debug.Log("1PCommandDel");
             }
         }
         if (timerCountTwo >= timer) {
             timerCountTwo =0;
-            if (twoPlayerCommandList.Count > 1) {
+            if (twoPlayerCommandList.Count > 2) {
                 twoPlayerCommandList.RemoveAt(0);
                 Debug.Log("2PCommandDel");
             }
         }
     }
+
+    
+    public char CheckCommandOnePlayer() {
+       
+        
+        char command = '5';
+
+        //コマンドの実装
+        int lengh = onePlayerCommandList.Count;
+        //Debug.Log(lengh);
+        if (onePlayerCommandList[lengh-1] == 6) {
+
+
+            command = 'a';
+        }
+        if(onePlayerCommandList[lengh-1] == 4) {
+
+
+            command = 'r';
+        }
+
+        return command;
+    }
+
+    public char CheckCommandTwoPlayer() {
+        
+        char command = '5';
+
+        //コマンドの実装
+        int lengh = twoPlayerCommandList.Count;
+        //Debug.Log(lengh);
+        if(twoPlayerCommandList[lengh-1] == 6) {
+
+
+            command = 'a';
+        }
+        if(twoPlayerCommandList[lengh-1] == 4) {
+
+
+            command = 'r';
+        }
+
+        return command;
+    }
+    
 
 }
