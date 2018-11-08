e2fbf1d8a3ed67855ba3e091ba542055f0628134
兎子-PC\兎子 <asahii.jr@gmail.com>
2018-11-08 22:24:27 +0900

    判定を作成

--- Assets/Script/Manager/StatusManeger.cs
+++ Assets/Script/Manager/StatusManeger.cs
@@ -125,17 +125,38 @@ public class StatusManeger : SingletonMonoBehavior<StatusManeger> {
         //コマンドの実装
         int lengh = commandlist.Count;
         //Debug.Log(lengh);
+        if (commandlist[lengh-1] == 8) {
+            command = 'j';
+        }
+        if (commandlist[lengh-1] == 2) {
+            command = 'd';
+        }
         if (commandlist[lengh-1] == 6) {
-
-
-            command = 'a';
+            command = 'r';
+            if (lengh >= 3) { return command; }
+            if (commandlist[lengh - 2] == 5 && commandlist[lengh - 3] == 6) {
+                command = 'S';
+            }
+        }
+        if (commandlist[lengh-1] == 3) {
+            command = 'e';
+        }
+        if (commandlist[lengh-1] == 9) {
+            command = 'c';
         }
         if(commandlist[lengh-1] == 4) {
-
-
-            command = 'r';
+            command = 'l';
+            if (lengh >= 3) { return command; }
+            if (commandlist[lengh - 2] == 5 && commandlist[lengh - 3] == 4) {
+                command = 's';
+            }
         }
-
+        if(commandlist[lengh-1] == 1) {
+            command = 'q';
+        }
+        if(commandlist[lengh-1] == 7) {
+            command = 'z';
+        }
         return command;
     }
 
