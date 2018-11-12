94f9cc1a3219e446dafc2aa17d8c46a8eecc78ab
sejimomizuki <sejimo.615@gmail.com>
2018-11-12 16:33:08 +0900

    ゲージをテキスト化（SingletonMonoBehaviorを使うと動かない）

--- Assets/Menber/Sejimo/CollisionChange.cs
+++ Assets/Menber/Sejimo/CollisionChange.cs
@@ -1,8 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CollisionChange
+public class CollisionChange : SingletonMonoBehavior<StatusManager>
 {
+    [SerializeField]
+    int maxDG = 100;
 
+    [SerializeField]
+    GameObject textObj;
+
+    Text text;
+    int onePgage;
+
+    private void Start()
+    {
+        Debug.Log("a");
+        text = textObj.GetComponent<Text>();
+        onePgage = StatusManager.Instance.DeathblowGuage[0];
+
+        text.text = onePgage.ToString();
+    }
 }
