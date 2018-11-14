220ae9d5529b6074f133a24d5f468ca516febd71
Sejimo <mizu.615@i.softbank.jp>
2018-11-14 10:18:23 +0900

    ゲージのテキスト完成

---
+++ Assets/Menber/Sejimo/CollisionChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChange {
+
+	// Use this for initialization
+	public void HitChange()
+    {
+        if (Input.GetKeyDown("under"))
+        {
+
+        }
+    }
+}
