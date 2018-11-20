90e9776e188b7e16a2d68ec5bb58c6ab3433e20d
Sejimo <mizu.615@i.softbank.jp>
2018-11-20 12:11:18 +0900

    当たり判定の処理（途中）

--- Assets/Menber/Sejimo/CollisionChange.cs
+++ Assets/Menber/Sejimo/CollisionChange.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionChange {
+public class CollisionChange : MonoBehaviour{
 
-	// Use this for initialization
-	public void HitChange()
+    // Use this for initialization
+    void FixedUpdate()
     {
+        Transform myTransform = this.transform;
+        Vector2 pos = myTransform.position;
+
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            this.transform.localScale += new Vector3(0, -0.5f, 0);
+            pos.y += -0.5f;
 
+            myTransform.position = pos;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            this.transform.localScale = new Vector2(0, 1);
         }
     }
 }
