e53432a5ada393ab56e0a620adc9e4ab2294df2f
sejimo <sejimo.615@gmail.com>
2018-11-29 18:21:49 +0900

    選択したプレイヤーの情報取得(途中)

--- Assets/Menber/Sejimo/PlayerSelect.cs
+++ Assets/Menber/Sejimo/PlayerSelect.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerSelect : MonoBehaviour {
 
+    //int oneP;
+
+    //int twoP;
+
+    GameObject player1;
+    GameObject player2;
+
+    PlayerManager script;
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,8 +24,39 @@ public class PlayerSelect : MonoBehaviour {
 		
 	}
 
-    public void OnClick()
-    {
+    //public void OnClick(int select)
+    //{
+    //    switch (select)
+    //    {
+    //        case 0:
+    //           // Debug.Log;
+    //            break;
 
+    //        case 1:
+    //           // Debug.Log;
+    //            break;
+    //    }
+    //}
+
+    void select ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player1 = GameObject.Find("player1");
+           // script = player1.GetComponent<PlayerManager.Player1>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            player2 = GameObject.Find("player1");
+          //  script = player1.GetComponent<PlayerManager.Player2>();
+        }
+    }
+
+    public void PlayerS()
+    {
+        //oneP = PlayerManager.
+
+        //twoP = PlayerManager.
     }
 }
