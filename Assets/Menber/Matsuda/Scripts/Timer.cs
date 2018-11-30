5b34001148ce7df0a18ba86b4c58dcfacbe0a48f
松田幸介 <darby8928@gmail.com>
2018-11-30 10:11:18 +0900

    カウントダウンのタイマー作成

---
+++ Assets/Menber/Matsuda/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]private float time;//タイマー
+    [SerializeField] private float downSpeed;//カウントを1減らすのにかかる時間
+    [SerializeField] T t;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime * downSpeed;
+        string str = time.ToString("F0");
+        t.TextUp(str);
+	}
+}
