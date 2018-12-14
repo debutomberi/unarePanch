5fed88777c2bae1e562a02218fbb09c3e538afb1
まつだ <darby8928@gmail.com>
2018-12-14 11:49:09 +0900

    select画面のポインターの移動とボタンを押した場所のオブジェクトの取得

---
+++ Assets/Menber/Matsuda/Scripts/Point.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Aボタンで選択");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown("joystick 2 button 1"))
+        {
+            if (collision.gameObject.tag == "2P" || collision.gameObject.tag == "1P") return;
+            Debug.Log(collision.gameObject.name);
+            Debug.Log("A");
+        }
+        if (Input.GetKeyDown("joystick 1 button 1"))
+        {
+            if (collision.gameObject.tag == "2P" || collision.gameObject.tag == "1P") return;
+            Debug.Log(collision.gameObject.name);
+            //Debug.Log("A");
+        }
+    }
+}
--- Assets/Menber/Matsuda/Scripts/PointerMove.cs
+++ Assets/Menber/Matsuda/Scripts/PointerMove.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PointerMove : MonoBehaviour {
-
+    [SerializeField] GameObject point1;
+    [SerializeField] GameObject point2;
+    [SerializeField] Vector2 x;
+    [SerializeField] Vector2 y;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal2") >= 0.5)
+        {
+            MovePointer(point2,x);
+        }
+        if (Input.GetAxis("Vertical2") >= 0.5)
+        {
+            MovePointer(point2,y);
+        }
+        if (Input.GetAxis("Horizontal2")<= -0.5)
+        {
+            MovePointer(point2,-x);
+        }
+        if (Input.GetAxis("Vertical2") <= -0.5)
+        {
+            MovePointer(point2,-y);
+        }
+        if (Input.GetAxis("Horizontal") >= 0.5)
+        {
+            MovePointer(point1,x);
+        }
+        if (Input.GetAxis("Vertical") >= 0.5)
+        {
+            MovePointer(point1,y);
+        }
+        if (Input.GetAxis("Horizontal") <= -0.5)
+        {
+            MovePointer(point1,-x);
+        }
+        if (Input.GetAxis("Vertical") <= -0.5)
+        {
+            MovePointer(point1,-y);
+        }
+    }
+
+    private void MovePointer(GameObject obj ,Vector3 i)
+    {
+        i = i / 10;
+        obj.transform.position += i;
+    }
 }
