e88f5175bd47c016536aa8eebed2dd2e0b47c3f3
まつだ <darby8928@gmail.com>
2018-12-14 13:00:39 +0900

    同じ処理が二回呼ばれる問題修正

--- Assets/Menber/Matsuda/Scripts/Point.cs
+++ Assets/Menber/Matsuda/Scripts/Point.cs
@@ -3,29 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Point : MonoBehaviour {
-
+    enum Controler
+    {
+        playerOne,
+        playerTwe
+    }
+    [SerializeField] Controler controler;
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Aボタンで選択");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+
+        switch (controler)
+        {
+            case Controler.playerOne:
+                if (Input.GetKeyDown("joystick 1 button 6"))
+                {
+                    Debug.Log("6 button");
+                }
+                if (Input.GetKeyDown("joystick 1 button 7"))
+                {
+                    Debug.Log("7 button");
+                }break;
+            case Controler.playerTwe:
+                if (Input.GetKeyDown("joystick 2 button 6"))
+                {
+                    Debug.Log("6 button");
+                }
+                if (Input.GetKeyDown("joystick 2 button 7"))
+                {
+                    Debug.Log("7 button");
+                }break;
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown("joystick 2 button 1"))
+        switch (controler)
         {
-            if (collision.gameObject.tag == "2P" || collision.gameObject.tag == "1P") return;
-            Debug.Log(collision.gameObject.name);
-            Debug.Log("A");
+            case Controler.playerOne:
+                if (Input.GetKeyDown("joystick 1 button 1"))
+                {
+                    Debug.Log("A");
+                    if (collision.gameObject.tag == "2P") return;
+                    Debug.Log(collision.gameObject.name);
+                }
+                break;
+            case Controler.playerTwe:
+                if (Input.GetKeyDown("joystick 2 button 1"))
+                {
+                    if (collision.gameObject.tag == "1P") return;
+                    Debug.Log(collision.gameObject.name);
+                }
+                break;
         }
-        if (Input.GetKeyDown("joystick 1 button 1"))
-        {
-            if (collision.gameObject.tag == "2P" || collision.gameObject.tag == "1P") return;
-            Debug.Log(collision.gameObject.name);
-            //Debug.Log("A");
-        }
+        
     }
 }
