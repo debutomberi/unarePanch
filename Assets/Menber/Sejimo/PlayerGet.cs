fed206baceaf5b80132de1c53fc017b8a7103f29
sejimo <sejimo.615@gmail.com>
2019-01-17 17:17:05 +0900

    データ保存（途中）

--- Assets/Menber/Sejimo/PlayerGet.cs
+++ Assets/Menber/Sejimo/PlayerGet.cs
@@ -1,37 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class PlayerGet : MonoBehaviour {
-
-    [SerializeField]
-    int PlayerID = 1;
+public class PlayerGet : ScriptableObject {
 
     [SerializeField]
-    GameObject pget;    // Inspector上に1Pのキャラクターを入れる
+    private int m_charaID;      // ID
+    [SerializeField]
+    private int Speed;        // スピード
+    [SerializeField]
+    private int Jump;         // ジャンプ力
+                                // 攻撃方法
 
-    PlayerSelect script;
-    
+    [MenuItem("Example/Create ExampleAsset Instance")]
+    static void CreateExampleAssetInstance()
+    {
+        var exampleAsset = CreateInstance<PlayerGet>();
+        AssetDatabase.CreateAsset(exampleAsset, "Assets/ExampleAsset.asset");
+        AssetDatabase.Refresh();
+    }
+
+    // キャラID取得
+    public int GetCharaID()
+    {
+        return m_charaID;
+    }
+
+    //[SerializeField]
+    //int PlayerID = 1;
+
+    //[SerializeField]
+    //GameObject pget;    // Inspector上に1Pのキャラクターを入れる
+
+    //PlayerSelect script;
+
 
     //Pselect = 0;
 
-	// Use this for initialization
-	void Start ()
-    {
 
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-
-    }
-
-    public void OnClick()
-    {
-        // PlayerSelect.csのPlayer関数を呼び出して1Pをsetする。
-        PlayerSelect.Instance.Player[PlayerID-1] = pget;
-    }
+    //public void OnClick()
+    //{
+    //    // PlayerSelect.csのPlayer関数を呼び出して1Pをsetする。
+    //    PlayerSelect.Instance.Player[PlayerID-1] = pget;
+    //}
 
 
 }
--- Assets/Menber/Sejimo/PlayerSporn.cs
+++ Assets/Menber/Sejimo/PlayerSporn.cs
@@ -2,11 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSporn : MonoBehaviour {
+public enum PLAYERID
+{
+    ID_1, // 1p
+    ID_2, // 2p
+};
+
+public class PlayerSporn {
+
+    static PlayerSporn m_instance = null;
+
+    int[] playerID = { -1, -1 };
+
+    static public PlayerSporn Instance
+    {
+        get
+        {
+            if(m_instance == null)
+            {
+                m_instance = new PlayerSporn();
+            }
+            return m_instance;
+        }
+    }
+    //↑他のクラスで呼び出すときにnew SelectManagerが返される
 
 
-    [SerializeField]
-    int playerID;
+    //選択したキャラクターの情報
+    public void SetPlayerSelectID(PLAYERID _pid, int _id)
+    {
+        playerID[(int)_pid] = _id;
+    }
+
+    public int GetPlayerSelectID(PLAYERID _pid)
+    {
+        return playerID[(int)_pid];
+    }
+
+
+    //[SerializeField]
+    //int playerID;
 
     PlayerSelect script;
     GameObject _player;     // playerの入れ物
@@ -15,28 +50,28 @@ public class PlayerSporn : MonoBehaviour {
     float jump;
     List<GameObject> ACOP = new List<GameObject>();
     PlayerManager pScript;
-    void Start () {
-	}
+ //   void Start () {
+	//}
 	
-	// Update is called once per frame
-	void Update ()
-    {
-        // PlayerGet.cs内でsetしたプレイヤーをgetする。
-        _player = PlayerSelect.Instance.Player[playerID - 1];
-        // _playerに何も入ってなかったらreturn
-        if (_player == null) return;
-        Vector3 pos = _player.transform.position;
-        _player.transform.position = new Vector3(-5, 0, 0);
-        Debug.Log(_player.transform.position);
+	//// Update is called once per frame
+	//void Update ()
+ //   {
+ //       // PlayerGet.cs内でsetしたプレイヤーをgetする。
+ //       _player = PlayerSelect.Instance.Player[playerID - 1];
+ //       // _playerに何も入ってなかったらreturn
+ //       if (_player == null) return;
+ //       Vector3 pos = _player.transform.position;
+ //       _player.transform.position = new Vector3(-5, 0, 0);
+ //       Debug.Log(_player.transform.position);
 
-        speed = pScript.Speed;
-        jump = pScript.Jump;
-        ACOP = pScript.attackColliderOnePlayer;
-        Debug.Log(speed + "スピード");
-        Debug.Log(jump + "ジャンプ");
-        Debug.Log(ACOP + "1P");
+ //       speed = pScript.Speed;
+ //       jump = pScript.Jump;
+ //       ACOP = pScript.attackColliderOnePlayer;
+ //       Debug.Log(speed + "スピード");
+ //       Debug.Log(jump + "ジャンプ");
+ //       Debug.Log(ACOP + "1P");
 
-    }
+ //   }
 
     void Awake()
     {
