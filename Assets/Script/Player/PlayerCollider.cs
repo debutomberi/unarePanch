93e3555fb419c36fe3c32b6a1c62b61bd388a9ed
asahi1998 <asahii.jr@gmail.com>
2019-01-25 17:26:21 +0900

    当たり判定の修正

---
+++ Assets/Script/Player/PlayerCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour {
+
+    public bool hit;
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.activeInHierarchy)
+        {
+            this.GetComponent<Rigidbody2D>().WakeUp();
+        }
+	}
+}
