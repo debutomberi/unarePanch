e9af68e406d3e14262eedff5c7c306ae6acb5f12
sejimo <mizu.615@i.softbank.jp>
2019-01-29 13:03:19 +0900

    Ready...GO! の実装

---
+++ Assets/Menber/Sejimo/ready_go.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ready_go : MonoBehaviour {
+
+    public GameObject startgo = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Ready");
+        startgo.GetComponent<Text>().enabled = true;
+    }
+
+
+    public IEnumerator Ready()
+    {
+        Time.timeScale = 0;
+        UnityEngine.UI.Text text = startgo.GetComponent<UnityEngine.UI.Text>();
+        text.text = "Ready";
+        Debug.Log("ready");
+        yield return new WaitForSecondsRealtime(2.0f);
+        text.text = "Go";
+        Debug.Log("go");
+        yield return new WaitForSecondsRealtime(1.0f);
+        text.text = "";
+        yield return new WaitForSecondsRealtime(0f);
+        Time.timeScale = 1;
+    }
+
+
+    // Update is called once per frame
+
+}
