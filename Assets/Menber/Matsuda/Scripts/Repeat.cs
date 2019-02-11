3dd89daf6ace436e558fa8bf04295ea3ee262eb8
松田幸介 <darby8928@gmail.com>
2019-02-11 23:37:32 +0900

    タイトルコール変更

---
+++ Assets/Menber/Matsuda/Scripts/Repeat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repeat : MonoBehaviour {
+    [SerializeField]AudioSource MainAudio;
+    [SerializeField] AudioSource SubAudio; 
+    int i = 0;
+    float time;
+	// Update is called once per frame
+	void Update () {
+        if (!SubAudio.isPlaying)
+        {
+            time += Time.deltaTime;
+            if (time >= 10)
+            {
+                time = 0;
+                MainAudio.volume = 0.15f;
+                SubAudio.Play();
+            }
+            else
+            {
+                MainAudio.volume = 1;
+            }
+        }
+	}
+}
